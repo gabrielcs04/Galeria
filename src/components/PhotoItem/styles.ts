79c3439918f71013c2067cea66a3aec8eaeffed8
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2021-10-07 13:50:21 -0300

    Subindo projeto no GitHub

---
+++ src/components/PhotoItem/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #3d3f43;
+  border-radius: 10px;
+  padding: 10px;
+
+  img {
+    max-width: 100%;
+    display: block;
+    margin-bottom: 10px;
+    border-radius: 10px;
+  }
+
+  button {
+    display: block;
+    background-color: #e10600;
+    border: 0;
+    color: #FFF;
+    padding: 8px 16px;
+    font-size: 15px;
+    border-radius: 10px;
+    margin: 10px auto 0 auto;
+    cursor: pointer;
+    transition: .2s;
+
+    &:hover {
+      opacity: .9;
+    }
+  }
+`;
